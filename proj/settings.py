70ed2aab75902eb6a1c8a2f8387255b5e1f837d0
Simar Sidhu <simarsidhu0102@gmail.com>
2024-08-24 12:31:15 +0530

    updated settings.py

--- proj/settings.py
+++ proj/settings.py
@@ -17,11 +17,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-<<<<<<< HEAD
-CSRF_TRUSTED_ORIGINS= [""]
-=======
->>>>>>> 1fd53bc81600c97dba14ba080ca23a590d233dfb
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,16 +64,13 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-<<<<<<< HEAD
-=======
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
->>>>>>> 1fd53bc81600c97dba14ba080ca23a590d233dfb
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -113,12 +105,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-<<<<<<< HEAD
 
 STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-=======
->>>>>>> 1fd53bc81600c97dba14ba080ca23a590d233dfb
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
